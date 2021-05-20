d5de2e6f4f72e309b6a071830169d820092f1b43
19036308 <19036308@myrp.edu.sg>
2021-05-20 12:13:15 +0800

    added user-permission with frag1 xml

--- app/src/main/java/sg/edu/rp/c346/id19036308/smsretriever/FragmentFirst.java
+++ app/src/main/java/sg/edu/rp/c346/id19036308/smsretriever/FragmentFirst.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentFirst extends Fragment {
+
+    Button btnAddTextFrag1;
+    EditText etSMSNum;
+    TextView tvFrag1;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +65,16 @@ public class FragmentFirst extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        tvFrag1 = view.findViewById(R.id.tvFrag1);
+        btnAddTextFrag1 = view.findViewById(R.id.btnRetrieveTextFrag1);
+
+        btnAddTextFrag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
     }
 }
