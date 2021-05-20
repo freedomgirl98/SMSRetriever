69edebf515ffbe1121539dec15677d976bb1ffd5
19036308 <19036308@myrp.edu.sg>
2021-05-20 12:44:03 +0800

    done frag1

--- app/src/main/java/sg/edu/rp/c346/id19036308/smsretriever/FragmentFirst.java
+++ app/src/main/java/sg/edu/rp/c346/id19036308/smsretriever/FragmentFirst.java
@@ -1,9 +1,16 @@
 package sg.edu.rp.c346.id19036308.smsretriever;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +27,7 @@ public class FragmentFirst extends Fragment {
 
     Button btnAddTextFrag1;
     EditText etSMSNum;
-    TextView tvFrag1;
+    TextView tvFrag1, tv2Frag1;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -67,12 +74,58 @@ public class FragmentFirst extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
         tvFrag1 = view.findViewById(R.id.tvFrag1);
+        tv2Frag1 = view.findViewById(R.id.tv2Frag1);
         btnAddTextFrag1 = view.findViewById(R.id.btnRetrieveTextFrag1);
 
         btnAddTextFrag1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int permissionCheck = PermissionChecker.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS);
 
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission
+                    // granted yet
+                    return;
+                }
+                //Create all messages URI
+                Uri uri = Uri.parse("content://sms");
+
+                // The columns we want
+                // date is when the message took place
+                // address is the number of the other party
+                // body is message content
+                // type 1 is received, type 2 sent
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                //Get Content Resolver object from which to
+                // query the content provider
+                ContentResolver cr = getActivity().getContentResolver();
+
+                // The filter String
+                String filter="body LIKE ? AND body LIKE ?";
+                // The matches for the ?
+                String[] filterArgs = {"%" + etSMSNum +"%"};
+                // Fetch SMS Message from Built-in Content Provider
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if(cursor.moveToFirst()){
+                    do{
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")){
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date + "\n\"" + body + "\"\n\n";
+
+                    } while (cursor.moveToNext());
+                }
+                tv2Frag1.setText(smsBody);
             }
         });
         return view;
